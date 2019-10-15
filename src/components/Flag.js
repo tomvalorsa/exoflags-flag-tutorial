b0b730440d8ac22f4d1d69390d36b4d257a71872
Tom Valorsa <tom.valorsa@arup.com>
2019-10-15 13:35:38 +0100

    Add basic scales and default props to flag, pass in scaled values to visual components

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { scaleLinear } from 'd3-scale';
 
 const FLAG_WIDTH = 500;
 const FLAG_HEIGHT= 300;
 
+const sizeScale = scaleLinear()
+  .domain([0, 100])
+  .range([0, 100]);
+
+const opacityScale = scaleLinear()
+  .domain([0, 100])
+  .range([0, 1]);
+
+const bgColorScale = scaleLinear()
+  .domain([0, 100])
+  .range(['#0000FF', '#FF0000']);
+
 const Wrapper = styled.div`
-  background-color: blue;
+  background-color: ${props => props.bgColor};
   /* 3:5 aspect ratio */
   height: ${FLAG_HEIGHT}px;
   width: ${FLAG_WIDTH}px;
@@ -18,10 +31,10 @@ const LeftTriangle = styled.div`
   top: 0;
   left: 0;
   height: 100%;
-  width: 50%;
+  width: ${props => props.size}%;
   border-style: solid;
-  border-width: ${FLAG_HEIGHT}px 0 0 ${FLAG_WIDTH}px;
-  border-color: transparent transparent transparent rgba(0, 0, 0, 1);
+  border-width: ${props => `${FLAG_HEIGHT}px 0 0 ${FLAG_WIDTH * (props.size / 100)}px`};
+  border-color: ${props => `transparent transparent transparent rgba(0, 0, 0, ${props.opacity})`};
 `;
 
 const RightTriangle = styled.div`
@@ -29,17 +42,23 @@ const RightTriangle = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
-  height: 50%;
+  height: ${props => props.size}%;
   width: 100%;
   border-style: solid;
-  border-width: ${FLAG_HEIGHT}px ${FLAG_WIDTH}px 0 0;
-  border-color: transparent rgba(255, 255, 255, 1) transparent transparent;
+  border-width: ${props => `${FLAG_HEIGHT * (props.size / 100)}px ${FLAG_WIDTH}px 0 0`};
+  border-color: ${props => `transparent rgba(255, 255, 255, ${props.opacity}) transparent transparent`};
 `;
 
-const Flag = () => (
-  <Wrapper>
-    <LeftTriangle />
-    <RightTriangle />
+const Flag = ({
+  leftSize = 50,
+  leftOpacity = 50,
+  rightSize = 50,
+  rightOpacity = 50,
+  bgColor = 50
+}) => (
+  <Wrapper bgColor={bgColorScale(bgColor)}>
+    <LeftTriangle size={sizeScale(leftSize)} opacity={opacityScale(leftOpacity)} />
+    <RightTriangle size={sizeScale(rightSize)} opacity={opacityScale(rightOpacity)} />
   </Wrapper>
 );
 
