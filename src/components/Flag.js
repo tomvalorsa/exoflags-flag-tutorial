0ac61fb128120dcec7ff6364ec1de9e34afb64b4
Tom Valorsa <tom.valorsa@arup.com>
2019-10-15 13:06:31 +0100

    Create basic layout and initial <Flag /> component

---
+++ src/components/Flag.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  background-color: blue;
+  /* 3:5 aspect ratio */
+  height: 300px;
+  width: 500px;
+`;
+
+const Flag = () => (
+  <Wrapper>
+
+  </Wrapper>
+);
+
+export default Flag;
---
+++ src/components/Page.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Page = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #16324F; /* coolors.co space cadet */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
+
+export default Page;
